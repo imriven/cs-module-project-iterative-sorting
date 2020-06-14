adfbb2b6eeb73dc559d9dec66304f42b95d76ff9
Christina Harris <christinathedesigner@outlook.com>
2020-06-14 11:39:05 -0700

    searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for index, item in enumerate(arr):
+        if item == target:
+            return index
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
 
-    # Your code here
+    while low <= high:
+        mid = (high + low) // 2
+        if target > arr[mid]:
+            low = mid + 1
+        elif target < arr[mid]:
+            high = mid - 1
+        else:
+            return mid
 
 
     return -1  # not found
